8b5b563581de2114c0d2b7ccc6a2f64c7ff6e285
Muhammad Mustafa Asif <mustafa.asif@commercetools.com>
2025-03-20 12:01:33 +0100

    feat: change site name

--- custom-view-config.mjs
+++ custom-view-config.mjs
@@ -2,15 +2,15 @@
  * @type {import('@commercetools-frontend/application-config').ConfigOptionsForCustomView}
  */
 const config = {
-  name: 'Mustafa Custom View',
+  name: 'Mustafa Prod Custom View',
   cloudIdentifier: 'gcp-eu',
   env: {
     development: {
       initialProjectKey: 'test-project-12121',
     },
     production: {
-      customViewId: 'cm8h71q85004fyd010ntrxux3',
-      url: 'https://comforting-alfajores-5abed2.netlify.app',
+      customViewId: 'cm8g1h3fn0018sm01allrpq37',
+      url: 'https://test-custom-view-prod.netlify.app',
     },
   },
   oAuthScopes: {
